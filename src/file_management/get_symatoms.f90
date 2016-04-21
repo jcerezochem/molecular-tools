a2b2a735bc141be98f245972005b0096a778c6a3
jcerezo <jcb1@um.es>
2016-04-21 21:14:43 +0200

    Add get_sym atoms to new-modules branch and read_scan
    Minor apps

--- src/file_management/get_symatoms.f90
+++ src/file_management/get_symatoms.f90
@@ -7,101 +7,132 @@ program get_symatoms
     !
     ! Description:
     ! -----------
-    ! Forms a gro structure with the one present in the fchk
+    ! Program to analyse vibrations in term of internal coordinates.
     !
     ! Compilation instructions (for mymake script):
-    !make$ echo "COMPILER: $FC"; sleep 1; $FC ../modules/alerts.f90 ../modules/structure_types_v2.f90 ../modules/line_preprocess.f90  ../modules/gro_manage_v2.f90 ../modules/pdb_manage_v2.f90 ../modules/constants_mod.f90 ../modules/gaussian_manage_v2.f90 ../modules/gaussian_fchk_manage_v2.f90 ../modules/xyz_manage.f90 ../modules/ff_build_module_v3.f90 fchk2gro_v2.1.f90 -o fchk2gro_v2.1.exe -cpp 
+    !make$ echo "COMPILER: $FC"; sleep 1; $FC ../modules/alerts.f90 ../modules/structure_types_v3.f90 ../modules/line_preprocess.f90 ../modules/ff_build_module_v3.f90 ../modules/gro_manage_v2.f90 ../modules/pdb_manage_v2.f90 ../modules/constants_mod.f90 ../modules/atomic_geom_v2.f90 ../modules/gaussian_manage_v2.f90 ../modules/gaussian_fchk_manage_v2.f90 ../modules/symmetry_mod.f90 ../modules/MatrixMod.f90 internal_SR_v6.f90 internal_duschinski_v5.f90 -llapack -o internal_duschinski_v5.exe -cpp -DDOUBLE
     !
     ! Change log:
-    !  July 2013: included xyz file format support
-    ! -- VERSION 2 --
-    !  July 2013: included std orientation from logs 
-    !             included -connect to create connections (for PDB only) -- needs: ff_build_module_v3.f90
-    ! -- VERSION 2.1 --
-    !  July 2013: guess connect is placed out of generic_strfile_read/write
-    !
-    !  Version 4: addapted to v4 modules
-    !  v4.1: add "swap" option (10/10/14)
-    !        included fcc (structure part of state file)
     !
     ! TODO:
     ! ------
     !
+    ! History
+    ! V3: Internal analysis is based on internal_duschinski_v5 (never finished...)
+    ! V4: Internal analysis is based on internal_duschinski_v7
+    !  V4b (not in the main streamline!): includes the generation of scans calc. for Gaussian.
+    !  V4c: the same as 4b. Bug fixes on guess_connect (ff_build module_v3 was buggy)
+    !
+    !Addapted to v4 release (distribution upgrade). Feb '14
+    !v4 releases:
+    !v4.0.1:
+    ! -use redundant coordinates
+    !v4.0.1.1:
+    ! - include UnSym (MOLCAS) file as input (freqs and nm)
+    !v4.0.1.2:
+    ! - if the calculation is only a internal scan, do not need the hessian (so do not try to read it)
+    !
     !============================================================================    
 
+
+    !*****************
+    !   MODULE LOAD
+    !*****************
+    !============================================
+    !   Generic
+    !============================================
     use alerts
-    use structure_types
     use line_preprocess
-    use gro_manage
-    use pdb_manage
-    use gaussian_manage
-    use gaussian_fchk_manage
+    use constants 
+    use verbosity
+    use matrix
+    use matrix_print
+    !============================================
+    !   Structure types module
+    !============================================
+    use structure_types
+    !============================================
+    !   File readers
+    !============================================
+    use generic_io
+    use generic_io_molec
     use xyz_manage
-    use molcas_unsym_manage
-    use ff_build
+    use gaussian_manage
+    !============================================
+    !  Structure-related modules
+    !============================================
     use molecular_structure
-    use symmetry_mod
+    use ff_build
+    use atomic_geom
+    use symmetry
+
 
     implicit none
 
+    integer,parameter :: NDIM = 600
+
     !====================== 
     !System variables
-    type(str_resmol) :: molec, &
-                        molec_aux
-    integer,dimension(1:1000) :: isym
+    type(str_resmol)  :: molecule
+    integer,dimension(1:NDIM) :: isym
+    integer :: Nat
+    !====================== 
+
+    !====================== 
+    !Auxiliar variables
+    integer :: error
+    character(1) :: null
     !====================== 
 
     !=============
-    !Counters and dummies
-    integer :: i,j,k,l, jj,kk, iat
-    character(len=1) :: null
-    logical :: overwrite=.false.,&
-               make_connect=.false.
-    !Swap related counters
-    integer :: iat_new, iat_orig, nswap
+    !Counters
+    integer :: i,j,k
     !=============
 
     !================
     !I/O stuff 
     !units
-    integer :: I_INP=10, &
-               I_SWP=11, &
-               O_OUT=20  
+    integer :: I_INP=10
     !files
-    character(len=5) :: resname="read"
-    character(len=10) :: filetype_inp="guess",&
-                         filetype_out="guess"
-    character(len=200):: inpfile="input.fchk",&
-                         outfile="default"   ,&
-                         swapfile="none"  
+    character(len=10) :: filetype="guess"
+    character(len=200):: inpfile ="input.fchk"
     !status
     integer :: IOstatus
-    character(len=7) :: stat="new" !do not overwrite when writting
     !===================
 
+    !===================
+    !CPU time 
+    real(8) :: ti, tf
+    !===================
+
+! (End of variables declaration) 
+!==================================================================================
+    call cpu_time(ti)
 
     ! 0. GET COMMAND LINE ARGUMENTS
-    call parse_input(inpfile,filetype_inp,outfile,filetype_out,overwrite,make_connect,resname,swapfile)
-
+    call parse_input(inpfile,filetype)
  
-    ! 1. READ INPUT
-    ! ---------------------------------
+    !================
+    ! READ DATA
+    !================
+    if (adjustl(filetype) == "guess") &
+    call split_line_back(inpfile,".",null,filetype)
+
+    ! STRUCTURE FILE
+    print'(X,A)', "READING STRUCTURE..."
     open(I_INP,file=inpfile,status='old',iostat=IOstatus)
     if (IOstatus /= 0) call alert_msg( "fatal","Unable to open "//trim(adjustl(inpfile)) )
+    call generic_strmol_reader(I_INP,filetype,molecule)
+    !Shortcuts
+    Nat = molecule%natoms
+    print'(X,A,/)', "Done"
 
-
-    if (adjustl(filetype_inp) == "guess") call split_line_back(inpfile,".",null,filetype_inp)
-    call generic_strfile_read(I_INP,filetype_inp,molec)
-    close(I_INP)
-    !Option to specify the resname from command line
-    if (adjustl(resname) /= "read") molec%atom(:)%resname=resname
-
-    ! 2. GUESS SYMMETRY AND WRITE
-    ! -------------------------------
-    call symm_atoms(molec,isym)
-    do i=1,molec%natoms
+    call symm_atoms(molecule,isym)
+    do i=1,molecule%natoms
         print*, i, isym(i)
     enddo
+
+
 
     stop
 
@@ -110,16 +141,13 @@ program get_symatoms
     contains
     !=============================================
 
-    subroutine parse_input(inpfile,filetype_inp,outfile,filetype_out,overwrite,make_connect,resname,swapfile)
+    subroutine parse_input(inpfile,filetype)
     !==================================================
     ! My input parser (gromacs style)
     !==================================================
         implicit none
 
-        character(len=*),intent(inout) :: inpfile,outfile,&
-                                          filetype_inp,filetype_out, &
-                                          resname,swapfile
-        logical,intent(inout) :: overwrite, make_connect
+        character(len=*),intent(inout) :: inpfile,filetype
         ! Local
         logical :: argument_retrieved,  &
                    need_help = .false.
@@ -137,31 +165,12 @@ program get_symatoms
                 case ("-f") 
                     call getarg(i+1, inpfile)
                     argument_retrieved=.true.
-                case ("-fti") 
-                    call getarg(i+1, filetype_inp)
+                case ("-ft") 
+                    call getarg(i+1, filetype)
+                    argument_retrieved=.true.  
+                case ("-thr") 
+                    call getarg(i+1, sym_thr)
                     argument_retrieved=.true.
-
-                case ("-o") 
-                    call getarg(i+1, outfile)
-                    argument_retrieved=.true.
-                case ("-fto") 
-                    call getarg(i+1, filetype_out)
-                    argument_retrieved=.true.
-
-                case ("-r")
-                    overwrite=.true.
-
-                case ("-rn")
-                    call getarg(i+1, resname)
-                    argument_retrieved=.true.
-
-                case ("-connect")
-                    make_connect=.true.
-
-                case ("-swap")
-                    call getarg(i+1, swapfile)
-                    argument_retrieved=.true.
-        
                 case ("-h")
                     need_help=.true.
 
@@ -170,115 +179,24 @@ program get_symatoms
             end select
         enddo 
 
-        if (adjustl(outfile) == "default") then
-            call split_line_back(inpfile,".",outfile,null)
-            outfile=trim(adjustl(outfile))//".gro"
-        endif
-
-        ! Some checks on the input
-        !----------------------------
-
-       !Print options (to stderr)
-        write(0,'(/,A)') '--------------------------------------------------'
-        write(0,'(/,A)') '        F O R M A T    C O N V E R T E R '    
-        write(0,'(/,A)') '        Convert between geometry formats '  
-        write(0,'(/,A)') '         Revision: fchk2gro-140225               '         
-        write(0,'(/,A)') '--------------------------------------------------'
-        write(0,*) '-f              ', trim(adjustl(inpfile))
-        write(0,*) '-fti            ', trim(adjustl(filetype_inp))
-        write(0,*) '-o              ', trim(adjustl(outfile))
-        write(0,*) '-fto            ', trim(adjustl(filetype_out))
-        write(0,*) '-r             ',  overwrite
-        write(0,*) '-swap           ',  trim(adjustl(swapfile))
-        write(0,*) '-rn             ',  trim(adjustl(resname))
-        write(0,*) '-connect       ',  make_connect
-        write(0,*) '-h             ',  need_help
-        write(0,*) '--------------------------------------------------'
+       !Print options (to stdx)
+        write(0,'(/,A)') '========================================================'
+        write(0,'(/,A)') '             G E T    S Y M A T O M S '    
+        call print_version()
+        write(0,'(/,A)') '========================================================'
+        write(0,'(/,A)') '-------------------------------------------------------------------'
+        write(0,'(A)')   ' Flag         Description                      Value'
+        write(0,'(A)')   '-------------------------------------------------------------------'
+        write(0,*)       '-f           Input file                       ', trim(adjustl(inpfile))
+        write(0,*)       '-ft          \_ FileTyep                      ', trim(adjustl(filetype))
+        write(0,*)       '-thr         Symm. thr [tight|normal|loose]   ', trim(adjustl(sym_thr))
+        write(0,*)       '-h           This help                       ',  need_help
+        write(0,*)       '-------------------------------------------------------------------'
         if (need_help) call alert_msg("fatal", 'There is no manual (for the moment)' )
 
         return
     end subroutine parse_input
-
-
-    subroutine generic_strfile_read(unt,filetype,molec)
-
-        integer, intent(in) :: unt
-        character(len=*),intent(inout) :: filetype
-        type(str_resmol),intent(inout) :: molec
-
-        !Local
-        type(str_molprops) :: props
-
-        ! Predefined filetypes
-        select case (adjustl(filetype))
-            case("gro")
-             call read_gro(unt,molec)
-            case("pdb")
-             call read_pdb_new(unt,molec)
-            case("g96")
-             call read_g96(unt,molec)
-            case("xyz")
-             call read_xyz(unt,molec)
-            case("log")
-             call parse_summary(unt,molec,props,"struct_only")
-             molec%atom(:)%resname = "UNK"
-             molec%atom(:)%resseq = 1
-            case("stdori")
-             call get_first_std_geom(unt,molec)
-             molec%atom(:)%resname = "UNK"
-             molec%atom(:)%resseq = 1
-            case("inpori")
-             call get_first_inp_geom(unt,molec)
-             molec%atom(:)%resname = "UNK"
-             molec%atom(:)%resseq = 1
-            case("fchk")
-             call read_fchk_geom(unt,molec)
-             molec%atom(:)%resname = "UNK"
-             molec%atom(:)%resseq = 1
-            case("UnSym")
-             call read_molcas_geom(unt,molec)
-            case default
-             call alert_msg("fatal","File type not supported: "//filetype)
-        end select
-
-        call atname2element(molec)
-
-        return
-
-    end subroutine generic_strfile_read
-
-
-    subroutine generic_strfile_write(unt,filetype,molec)
-
-        integer, intent(in) :: unt
-        character(len=*),intent(inout) :: filetype
-        type(str_resmol),intent(inout) :: molec
-
-        ! Predefined filetypes
-        select case (adjustl(filetype))
-            case("gro")
-             call write_gro(unt,molec)
-            case("pdb")
-             call write_pdb(unt,molec)
-            case("pdb-c")
-             call write_pdb_connect(unt,molec)
-            case("g96")
-             call write_g96(unt,molec)
-            case("xyz")
-             call write_xyz(unt,molec)
-            case("fchk")
-             call element2AtNum(molec)
-             call write_fchk_geom(unt,molec)
-            case("fcc")
-             call write_fcc(unt,molec)
-            case default
-             call alert_msg("fatal","File type not supported: "//filetype)
-        end select
-
-        return
-
-    end subroutine generic_strfile_write 
-
+       
 
 end program get_symatoms
 
