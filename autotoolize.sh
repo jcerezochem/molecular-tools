ad6eaaede62588e0179f96f791910412ea8f2150
jcerezo <jcb1@um.es>
2016-05-04 13:38:28 +0200

    Solve issues to load MKL libs
    The macro: AC_CHECK_LIB (library, function, [action-if-found], [action-if-not-found], [other-libraries])
    is now used properly, adding the addtional mkl libs as [other-libraries]

--- autotoolize.sh
+++ autotoolize.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-rm configure; aclocal; autoconf; sed -i "s/-lmkl_intel/-lmkl_intel_lp64 -lmkl_sequential -lmkl_core/g" configure; automake -a
+rm configure; aclocal; autoconf; automake -a
+#rm configure; aclocal; autoconf; sed -i "s/-lmkl_intel/-lmkl_intel_lp64 -lmkl_sequential -lmkl_core/g" configure; automake -a
 
 
