jcerezochem/molecular-tools

commit 1f68dc1961bdc737bc1aa5ed05601d3584b248cf

Improve support to Gradient from log
* A new SR in gaussian_manage reads the forces
  with the log (NOT from the summary)
  This is useful to get the forces from a Force
  calculation, since in this case, it is not
  added to the summary. Used by reform_fchk

src/file_management/reform_fchk.f90
@@ -95,7 +95,7 @@ program reorder_fchk
 
     !=====================
     ! Available data swithes
-    logical :: have_gradient, have_hessian,&
+    logical :: have_gradient, have_hessian, read_log_body,&
                ! Energies
                have_SCF,have_TD,have_TOT
     !===================== 
@@ -180,16 +180,6 @@ program reorder_fchk
     if (adjustl(filetype) /= "log" .and. adjustl(filetype) /= "fchk") &
         call alert_msg("fatal","Only Gaussian log and fchk files supported")
 
-    ! STRUCTURE FILE
-    print'(X,A)', "READING STRUCTURE..."
-    open(I_INP,file=inpfile,status='old',iostat=IOstatus)
-    if (IOstatus /= 0) call alert_msg( "fatal","Unable to open "//trim(adjustl(inpfile)) )
-    call generic_strmol_reader(I_INP,filetype,molecule)
-    !Shortcuts
-    Nat = molecule%natoms
-    Nvib = 3*Nat-6
-    print'(X,A,/)', "Done"
-
     ! JOB INFO
     print'(X,A)', "READING JOB INFO..."
     rewind(I_INP)
@@ -203,9 +193,46 @@ program reorder_fchk
     print'(X,A,I0)', " Mult.   : ", mult
     print'(X,A,/)', "Done"
 
-    ! HESSIAN FILE
-    have_hessian=.false.
+    ! Determine the available data and how to read them (Freq and Grad)
+    have_gradient=.false.
     if (adjustl(calc_type) == "Freq") then
+        ! Freq calcs have gradient in log and fchk
+        have_gradient=.true.
+        have_hessian=.true.
+        read_log_body=.false.
+    elseif (adjustl(calc_type) == "Opt"   .or. &
+            adjustl(calc_type) == "FOpt"  .or. &
+            adjustl(calc_type) == "Force") then
+!       ! This calculation types do not have the gradient in the log summary
+        have_gradient=.true.
+        have_hessian=.false.
+        read_log_body=.true.
+    endif
+
+    ! STRUCTURE FILE
+    print'(X,A)', "READING STRUCTURE..."
+    open(I_INP,file=inpfile,status='old',iostat=IOstatus)
+    if (IOstatus /= 0) call alert_msg( "fatal","Unable to open "//trim(adjustl(inpfile)) )
+    call generic_strmol_reader(I_INP,filetype,molecule)
+    !Shortcuts
+    Nat = molecule%natoms
+    Nvib = 3*Nat-6
+    print'(X,A,/)', "Done"
+
+    ! GRADIENT FILE
+    if (have_gradient) then
+        rewind(I_INP)
+        print'(X,A)', "READING GRADIENT..."
+        if (read_log_body) then
+            call read_gausslog_forces(I_INP,Nat,Grad,error)
+        else
+            call generic_gradient_reader(I_INP,filetype,Nat,Grad,error)
+        endif
+        print'(X,A,/)', "Done"
+    endif
+
+    ! HESSIAN FILE
+    if (have_hessian) then
         rewind(I_INP)
         print'(X,A)', "READING HESSIAN..."
         allocate(Hlt(1:3*Nat*(3*Nat+1)/2))
@@ -213,28 +240,6 @@ program reorder_fchk
         Hess(1:3*Nat,1:3*Nat) = Hlt_to_Hess(3*Nat,Hlt)
         print'(X,A,/)', "Done"
         have_hessian=.true.
-    endif
-
-    ! GRADIENT FILE
-    have_gradient=.false.
-    if (adjustl(calc_type) == "Freq") then
-        ! Freq calcs have gradient in log and fchk
-        have_gradient=.true.
-    elseif (adjustl(calc_type) == "Opt"   .or. &
-            adjustl(calc_type) == "FOpt"  .or. &
-            adjustl(calc_type) == "Force") then
-        ! Other calcs only have gradient on fchk
-        if (adjustl(filetype) == "fchk") then
-            have_gradient=.true.
-        else
-            have_gradient=.false.
-        endif
-    endif
-    if (have_gradient) then
-        rewind(I_INP)
-        print'(X,A)', "READING GRADIENT..."
-        call generic_gradient_reader(I_INP,filetype,Nat,Grad,error) 
-        print'(X,A,/)', "Done"
     endif
 
     ! ENERGIES

src/modules/generic_io.f90
@@ -36,7 +36,7 @@ module generic_io
 
         !Local
         integer :: i
-        character(len=len(AtName(i))) :: element
+        character(len=len(AtName(1))) :: element
         
         do i=1,Nat
             element = element_from_AtName(AtName(i))

src/modules/manage_gaussian.f90
@@ -1061,6 +1061,78 @@ module gaussian_manage
 
     end subroutine read_gaussfchk_dip
 
+    subroutine read_gausslog_forces(unt,Nat,Grad,error_flag)
+
+        !=====================================================
+        ! THIS CODE IS PART OF FCC_TOOLS
+        !=====================================================
+        ! Description
+        !  Read Forces (gradient) from the log file for a Force
+        !  calculation. NOT FROM SUMMARY, but from the middle
+        !  of the log
+        !
+        ! Notes
+        !  Only the length-gauge reponse properties are taken
+        !========================================================
+
+        integer,intent(in)              :: unt
+        integer,intent(in)              :: Nat
+        real(8),dimension(:),intent(out):: Grad
+        integer,intent(out),optional    :: error_flag
+
+        !Local
+        !Variables for reading
+        character(len=240)               :: line=""
+        character(len=3)                 :: auxchar
+        !I/O
+        integer :: IOstatus
+        !Other local
+        integer                          :: i,j,k, ii, jj
+
+        ! Number of excited states computed
+        ! Search section
+        print*, "reading gradient from log body"
+        ii = 0
+        if (present(error_flag)) error_flag = 0
+        do
+                ii = ii + 1
+                read(unt,'(A)',IOSTAT=IOstatus) line
+                ! Possible scenarios while reading:
+                ! 1) End of file
+                if ( IOstatus < 0 ) then
+                    if (present(error_flag)) error_flag = -ii
+                    return
+                endif
+                ! 2) Found a Excited State section
+                if ( INDEX(line,"Forces (Hartrees/Bohr)") /= 0 ) then
+                    exit
+                endif
+                ! 3) That was the target state
+                if (INDEX(line,"GINC")/=0) then
+                    call alert_msg("note","No Force inside the log")
+                    if (present(error_flag)) error_flag = 1
+                    return
+                endif
+        enddo
+
+        ! Read next useless line
+        read(unt,'(A)',IOSTAT=IOstatus) line
+
+        ! Read gradient
+        do i=1,Nat
+            j=3*(i-1)
+            read(unt,*,IOSTAT=IOstatus) ii,ii, Grad(j+1), Grad(j+2), Grad(j+3)
+            if ( IOstatus < 0 ) then
+                call alert_msg("warnign","Unexpected end-of-section while reading Forces (log)")
+                if (present(error_flag)) error_flag = 1
+                return
+            endif
+        enddo
+
+        return
+
+    end subroutine read_gausslog_forces
+
     subroutine read_gausslog_targestate(unt,S,error_flag)
 
         !=====================================================

commit 9895805b4920cd87473125a214aed954da0cb554

Fix projection in nm_internal reform_fchk (broken)
* Fix the projection-out of the internal coordinates along with
  the vertical method. Now -prj-trS and -intmode0 all provide
  the same results

* reform_fchk was broken from commit 1f68dc1961bd, since the
  file was tried to be read before opeing after reordening the
  actions in the code

src/file_management/reform_fchk.f90
@@ -174,6 +174,9 @@ program reorder_fchk
     !================
     ! READ DATA
     !================
+    ! OPEN FILE
+    open(I_INP,file=inpfile,status='old',iostat=IOstatus)
+    if (IOstatus /= 0) call alert_msg( "fatal","Unable to open "//trim(adjustl(inpfile)) )
     if (adjustl(filetype) == "guess") &
     call split_line_back(inpfile,".",null,filetype)
 
@@ -211,8 +214,7 @@ program reorder_fchk
 
     ! STRUCTURE FILE
     print'(X,A)', "READING STRUCTURE..."
-    open(I_INP,file=inpfile,status='old',iostat=IOstatus)
-    if (IOstatus /= 0) call alert_msg( "fatal","Unable to open "//trim(adjustl(inpfile)) )
+    rewind(I_INP)
     call generic_strmol_reader(I_INP,filetype,molecule)
     !Shortcuts
     Nat = molecule%natoms

src/normal_modes/normal_modes_internal.f90
@@ -75,7 +75,7 @@ program normal_modes_animation
     integer,dimension(1:NDIM) :: isym
     integer,dimension(4,1:NDIM,1:NDIM) :: Osym
     integer :: Nsym
-    integer :: Nat, Nvib, Ns, Nvib0
+    integer :: Nat, Nvib, Ns, Nvib0, NvibP
     integer :: Ns_zmat, Ns_all, Nvib_all
     character(len=5) :: PG
     real(8) :: Tthermo=0.d0
@@ -384,8 +384,22 @@ program normal_modes_animation
 
         !---------------------------------------
         ! NOW, GET THE ACTUAL WORKING INTERNAL SET
+        call define_internal_set(molecule,"ALL",intfile,rmzfile,use_symmetry,isym,S_sym,Ns)
+        allgeom = molecule%geom
         call define_internal_set(molecule,def_internal0,intfile0,rmzfile,use_symmetry,isym,S_sym,Ns)
         !---------------------------------------
+        
+        ! Compute B matrix in the selgeom for projection
+        if (apply_projection_matrix) then
+            NvibP = Nvib
+            call internal_Wilson(molecule,Ns,S,Bprj)
+            call internal_Gmetric(Nat,Ns,molecule%atom(:)%mass,Bprj,G)
+            call redundant2nonredundant(Ns,NvibP,G,Asel)
+            Bprj(1:Nvib_all,1:3*Nat) = matrix_product(NvibP,3*Nat,Ns,Asel,Bprj,tA=.true.)
+            ! And get back allgeom
+            Ns = allgeom%nbonds+allgeom%nangles+allgeom%ndihed
+            molecule%geom = allgeom
+        endif
 
         ! Get G, B, and Bder 
         call internal_Wilson(molecule,Ns,S,B0,ModeDef)
@@ -408,11 +422,12 @@ program normal_modes_animation
 
         if (apply_projection_matrix) then
             ! Get projection matrix (again...)
-            P(1:3*Nat,1:3*Nat) = projection_matrix3(Nat,Nvib0,B0,molecule%atom(:)%Mass)
+!             P(1:3*Nat,1:3*Nat) = projection_matrix3(Nat,Nvib0,B0,molecule%atom(:)%Mass)
+            P(1:3*Nat,1:3*Nat) = projection_matrix3(Nat,NvibP,Bprj,molecule%atom(:)%Mass)
             ! And rotate gradient
             do i=1,3*Nat
                 Vec1(i) = 0.d0
-                do k=1,Nvib0
+                do k=1,3*Nat
                     Vec1(i) = Vec1(i) + P(i,k)*Grad(k)
                 enddo
             enddo
@@ -443,33 +458,7 @@ program normal_modes_animation
         endif
 
         if (check_symmetry) then
-            print'(/,X,A)', "---------------------------------------"
-            print'(X,A  )', " Check effect of symmetry operations"
-            print'(X,A  )', " on the correction term gs^t\beta"
-            print'(X,A  )', "---------------------------------------"
-            current_symm=molecule%PG
-            molecule%PG="XX"
-            call symm_atoms(molecule,isym,Osym,rotate=.false.,nsym_ops=nsym)
-            ! Check the symmetry of the correction term
-            ! Check all detected symmetry ops
-            do iop=1,Nsym
-                Aux(1:3*Nat,1:3*Nat) = dfloat(Osym(iop,1:3*Nat,1:3*Nat))
-                Aux(1:3*Nat,1:3*Nat) = matrix_basisrot(3*Nat,3*Nat,Aux,gBder,counter=.true.)
-                Theta=0.d0
-                do i=1,3*Nat 
-                do j=1,3*Nat 
-                    if (Theta < abs(Aux(i,j)-gBder(i,j))) then
-                        Theta = abs(Aux(i,j)-gBder(i,j))
-                        Theta2=gBder(i,j)
-                    endif
-                enddo
-                enddo
-                print'(X,A,I0)', "Symmetry operation :   ", iop
-                print'(X,A,F10.6)',   " Max abs difference : ", Theta
-                print'(X,A,F10.6,/)', " Value before sym op: ", Theta2
-            enddo
-            print'(X,A,/)', "---------------------------------------"
-            molecule%PG=current_symm
+            call check_symm_gsBder(molecule,gBder)
         endif
     endif
 
@@ -534,11 +523,11 @@ program normal_modes_animation
 
     ! Compute B matrix in the selgeom for projection
     if (apply_projection_matrix) then
-        Nvib0 = Nvib
+        NvibP = Nvib
         call internal_Wilson(molecule,Ns,S,Bprj)
         call internal_Gmetric(Nat,Ns,molecule%atom(:)%mass,Bprj,G)
-        call redundant2nonredundant(Ns,Nvib0,G,Asel)
-        Bprj(1:Nvib_all,1:3*Nat) = matrix_product(Nvib0,3*Nat,Ns,Asel,Bprj,tA=.true.)
+        call redundant2nonredundant(Ns,NvibP,G,Asel)
+        Bprj(1:Nvib_all,1:3*Nat) = matrix_product(NvibP,3*Nat,Ns,Asel,Bprj,tA=.true.)
         ! And get back allgeom
         Ns = allgeom%nbonds+allgeom%nangles+allgeom%ndihed
         molecule%geom = allgeom
@@ -684,7 +673,7 @@ program normal_modes_animation
 
             if (apply_projection_matrix) then
                 ! Get projection matrix (again...)
-                P(1:3*Nat,1:3*Nat) = projection_matrix3(Nat,Nvib0,Bprj,molecule%atom(:)%Mass)
+                P(1:3*Nat,1:3*Nat) = projection_matrix3(Nat,NvibP,Bprj,molecule%atom(:)%Mass)
 !                 Aux(1:3*Nat,1:3*Nat) = identity_matrix(3*Nat)
 !                 P(1:3*Nat,1:3*Nat) =  Aux(1:3*Nat,1:3*Nat)-P(1:3*Nat,1:3*Nat)
                 ! Project out rotation and translation